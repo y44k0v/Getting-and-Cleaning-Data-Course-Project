57792f3cc87b2b1eca94ee0302dc7233e88efafc
Yaakov Yosef Miller <ykv00001@gmail.com>
2017-03-24 15:02:12 -0400

    file addition

---
+++ run_analysis.R
@@ -0,0 +1,50 @@
+library(plyr)
+
+## Gathering the data 
+fileUrl<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,"w4.zip", method = "curl")
+unzip("w4.zip")
+
+## Reading activity labels, features, test and training data
+
+activityLabels<-read.table("./UCI HAR Dataset/activity_labels.txt")
+
+features<-read.table("./UCI HAR Dataset/features.txt")
+
+xTest<-read.table("./UCI HAR Dataset/test/X_test.txt")
+yTest<-read.table("./UCI HAR Dataset/test/y_test.txt")
+subjectTest<-read.table("./UCI HAR Dataset/test/subject_test.txt")
+
+xTrain<-read.table("./UCI HAR Dataset/train/X_train.txt")
+yTrain<-read.table("./UCI HAR Dataset/train/y_train.txt")
+subjectTrain<-read.table("./UCI HAR Dataset/train/subject_train.txt")
+
+## Consolidating Data 
+
+xData<-rbind(xTest, xTrain)
+yData<-rbind(yTest, yTrain)
+subject<-rbind(subjectTest, subjectTrain)
+
+## Selecting measurements on the mean and standard deviation 
+
+# Assigning features to data 
+colnames(xData)<-features[,2]
+
+xmean<-grep("mean\\(\\)",features[,2])
+xstd<-grep("std\\(\\)",features[,2])
+xData<-xData[,c(xmean,xstd)]
+
+## Assigning descriptive activity names 
+
+yData[, 1] <- activityLabels[yData[, 1], 2]
+names(yData) <- "activity"
+names(subject) <- "subject"
+
+## Merging all the data
+
+data<-cbind(yData,subject,xData)
+
+## Average values for activity and subject
+
+meanData <- ddply(data, .(subject, activity), function(x) colMeans(x[, 2:68]))
+head(meanData, 20)
